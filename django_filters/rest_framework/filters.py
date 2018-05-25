16612baff2caca7ebaa4549e503d2708a084a5d0
Luci Furtun <lucianfurtun@gmail.com>
2018-05-25 14:35:04 +0300

    Add more fields

--- django_filters/rest_framework/filters.py
+++ django_filters/rest_framework/filters.py
@@ -1,11 +1,47 @@
 from rest_framework import fields
 
-from ..filters import *
+from .. import filters
 
 
-class BooleanFilter(BooleanFilter):
+class CharFilter(filters.CharFilter):
+    field_class = fields.CharField
+
+
+class BooleanFilter(filters.BooleanFilter):
     field_class = fields.BooleanField
 
 
-class NumberFilter(NumberFilter):
+class NumberFilter(filters.NumberFilter):
     field_class = fields.FloatField
+
+
+class ListFilter(filters.MultipleChoiceFilter):
+    field_class = fields.ListField
+
+
+class ChoiceFilter(filters.ChoiceFilter):
+    field_class = fields.ChoiceField
+
+
+class UUIDFilter(filters.UUIDFilter):
+    field_class = fields.UUIDField
+
+
+class MultipleChoiceFilter(filters.MultipleChoiceFilter):
+    field_class = fields.MultipleChoiceField
+
+
+class DateFilter(filters.DateFilter):
+    field_class = fields.DateField
+
+
+class DateTimeFilter(filters.DateTimeFilter):
+    field_class = fields.DateTimeField
+
+
+class TimeFilter(filters.TimeFilter):
+    field_class = fields.TimeField
+
+
+class DurationFilter(filters.DurationFilter):
+    field_class = fields.DurationField
